0ff51f4f7b7d5df6fa7a98eb126aa64c1cb09514
ayounsni <Ayoubsnini@gmail.com>
2025-02-24 16:34:06 +0000

    Ajout et amélioration du design et de l'authentification
    - Ajout de l'image 'bird.png' (public/assets/)
    - Création du composant Auth (auth.component.css, auth.component.html, auth.component.ts)
    - Création du composant Home (home.component.css, home.component.html, home.component.ts)
    - Amélioration du design de la navbar (CSS, HTML, TypeScript)
    - Mise à jour du footer (footer.component.html)
    - Mise à jour des routes (app.routes.ts)
    - Ajustements du style et structure de app.component.html

--- molou3_Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ molou3_Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -9,7 +9,6 @@ import { Component, HostListener } from '@angular/core';
 })
 export class NavbarComponent {
   imagepath = 'assets/logo.png'; 
-  image = 'assets/ciel.png'; 
   isDropdownOpen = false;
 
   toggleDropdown() {
@@ -19,24 +18,12 @@ export class NavbarComponent {
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
     if (this.isDropdownOpen) {
-      // Vérifie si le clic est à l'intérieur du bouton ou du dropdown
       const target = event.target as HTMLElement;
-      const clickedInside = target.closest('.dropdown-trigger') || target.closest('.dropdown-menu');
-      
-      // Ferme le dropdown si le clic est en dehors
+      const clickedInside = target.closest('.dropdown-trigger') || target.closest('.dropdown-menu');  
       if (!clickedInside) {
         this.isDropdownOpen = false;
       }
     }
   }
 
-  activateHoverEffect(event: MouseEvent) {
-    const button = event.currentTarget as HTMLElement;
-    const rect = button.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    button.style.setProperty('--x', `${x}px`);
-    button.style.setProperty('--y', `${y}px`);
-  }
-  
 }
