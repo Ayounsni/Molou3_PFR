6a012e36dc709f79ebbe5ba7e266938a77204c8a
ayounsni <Ayoubsnini@gmail.com>
2025-02-28 12:47:00 +0000

    Mise à jour de la navbar et de la page d'accueil

--- molou3_Frontend/src/app/shared/features/home/home.component.ts
+++ molou3_Frontend/src/app/shared/features/home/home.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  image = 'assets/bird.png'; 
+  image = 'assets/image.png'; 
   logo= 'assets/molo.png';
   meteo= 'assets/meteo2.png';
   rank= 'assets/rank2.png';
   plan= 'assets/plan1.jpg';
   market= 'assets/market1.png';
+  verified = 'assets/verified.png';
+
+  cyclists = [
+    { name: 'Teniel Campbell', image: 'assets/association.png' },
+    { name: 'Ayesha McGowan', image: 'assets/association.png' },
+    { name: 'Kate Courtney', image: 'assets/association.png' },
+    { name: 'Demi Vollering', image: 'assets/association.png' },
+    { name: 'Demi Vollering', image: 'assets/association.png' },
+    { name: 'Demi Vollering', image: 'assets/association.png' },
+  ];
+
 
 }
