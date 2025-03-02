59d57acc743af49aa8e66b390d2cb5ccc14710aa
ayounsni <Ayoubsnini@gmail.com>
2025-03-02 18:46:05 +0000

    Ajout de la page météo (HTML, CSS, TS) et mise à jour du routage. Ajout de nouvelles images dans les assets.

--- molou3_Frontend/src/app/app.routes.ts
+++ molou3_Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/features/home/home.component';
+import { MeteoComponent } from './shared/features/meteo/meteo.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'meteo', component: MeteoComponent },
 ];
---
+++ molou3_Frontend/src/app/shared/features/meteo/meteo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-meteo',
+  imports: [FooterComponent],
+  templateUrl: './meteo.component.html',
+  styleUrl: './meteo.component.css'
+})
+export class MeteoComponent {
+
+}
